9e9a274c8ed9acea7b8d5185bfc332c48a5243ba
James Russell <fizwidget@gmail.com>
2020-03-04 19:46:27 +1100

    Delete unnecessary stuff

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { Page } from "./Page";
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: "http://localhost:8080/graphql" }),
   cache: new InMemoryCache()
 });
 
-interface Props {
-  children: JSX.Element;
-}
-
-export default ({ children }: Props) => (
-  <ApolloProvider client={client}>{children}</ApolloProvider>
+export const App = () => (
+  <ApolloProvider client={client}>
+    <Page />
+  </ApolloProvider>
 );
